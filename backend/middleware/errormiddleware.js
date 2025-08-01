7edb5413e02c438947a40530f72a281a3d521950
sathya Narayana <47709520+jaderaju@users.noreply.github.com>
2025-08-01 15:47:18 +0300

    Update errormiddleware.js

--- backend/middleware/errormiddleware.js
+++ backend/middleware/errormiddleware.js
@@ -2,10 +2,9 @@
 
 const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode && res.statusCode !== 200 ? res.statusCode : 500;
-  res.status(statusCode);
-  res.json({
+  res.status(statusCode).json({
     message: err.message,
-    stack: process.env.NODE_ENV === 'production' ? undefined : err.stack,
+    stack: process.env.NODE_ENV === 'production' ? undefined : err.stack
   });
 };
 
